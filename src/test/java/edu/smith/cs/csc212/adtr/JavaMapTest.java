8f7a116b9fc70d6b7b2e03286fff8bd5aa3de37b
Isabel-Zamudio-Moore <izamudiomoore@smith.edu>
2019-10-04 16:13:40 -0300

    Feature(s): New Tests for Set and Map

--- src/test/java/edu/smith/cs/csc212/adtr/JavaMapTest.java
+++ src/test/java/edu/smith/cs/csc212/adtr/JavaMapTest.java
@@ -1,21 +1,110 @@
 package edu.smith.cs.csc212.adtr;
 
 import static org.junit.Assert.*;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 import edu.smith.cs.csc212.adtr.real.JavaMap;
 
 public class JavaMapTest {
 	
+	/**
+	 *  Allow us to test that an integer
+	 *  is equal with another integer
+	 */
 	// you might want this.
 	void assertIntEq(int x, int y) {
 		assertEquals(x, y);
 	}
 
+	/**
+	 *  Test that empty map is empty
+	 *  The test should have no errors or failures
+	 *  if the empty map is empty
+	 */
 	@Test
 	public void testEmpty() {
 		MapADT<String, String> empty = new JavaMap<>();
 		assertEquals(empty.size(), 0);
 		//Assert.assert
+	}
+	
+	
+	/**
+	 *  Test that put method is working
+	 *  The test should have no errors or failures
+	 *  if the put method is putting items into map
+	 */
+	@Test
+	public void testPut() {
+		MapADT<String, Integer> Put= new JavaMap<>();
+		Put.put("YourScore",10);
+		Put.put("T's Score",14);
+		Put.put("L's Score",19);
+		assertEquals(Put.size()==3, true);
+		assertEquals(Put.size()==1, false);
+
+		
+	}
+	
+	/**
+	 *  Test that get method is working
+	 *  The test should have no errors or failures
+	 *  if the get method is telling us the
+	 *  corresponding value to the key we want
+	 */
+	
+	@Test
+	public void testGet() {
+		MapADT<String, Integer> Get= new JavaMap<>();
+		Get.put("Your Score",12);
+		Get.put("T's Score", 14);
+		Get.put("L's Score", 18);
+		assertEquals((int) Get.get("T's Score"), (int) 14);
+		assertEquals(Get.get("T's Score")==20 , false);
+		assertEquals(Get.get("L's Score")==18 , true);
+		assertEquals(Get.get("L's Score")==17 , false);
+		assertEquals(Get.get("Your Score")==12 , true);
+		assertEquals(Get.get("Your Score")==20 , false);
+		
+	}
+	
+	/**
+	 *  Test that size method is working
+	 *  The test should have no errors or failures
+	 *  if the size is what we expect it to be
+	 */
+	@Test
+	public void testSize() {
+		MapADT<String, Integer> Size= new JavaMap<>();
+		Size.put("Your Score",15);
+		Size.put("T's Score", 16);
+		Size.put("L's Score", 17);
+		assertEquals(Size.size()==3, true);
+		assertEquals(Size.size()==6, false);
+		
+	}
+	
+	
+	/**
+	 *  Test that remove method is working
+	 *  The test should have no errors or failures
+	 *  if the remove method is removing the item we 
+	 *  ask to remove
+	 */
+	@Test
+	public void testRemove() {
+		MapADT<String, Integer> Remove= new JavaMap<>();
+		Remove.put("Your Score",30);
+		Remove.put("T's Score", 20);
+		Remove.put("L's Score", 20);
+		assertIntEq(Remove.remove("Your Score"), 30);
+		//assertEquals(Remove.size()==7, false);
+		
+		
+		
+		
+		
 	}
 }
--- src/test/java/edu/smith/cs/csc212/adtr/JavaSetTest.java
+++ src/test/java/edu/smith/cs/csc212/adtr/JavaSetTest.java
@@ -1,6 +1,8 @@
 package edu.smith.cs.csc212.adtr;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -8,10 +10,101 @@ import edu.smith.cs.csc212.adtr.real.JavaSet;
 
 public class JavaSetTest {
 	
+	/**
+	 *  Test that empty set is empty
+	 *  The test should have no errors or failures
+	 *  if the size method works
+	 */
 	@Test
 	public void testEmpty() {
 		SetADT<String> empty = new JavaSet<>();
 		assertEquals(empty.size(), 0);
 	}
+	
+	/**
+	 *  Test that size method is working
+	 *  The test should have no errors or failures
+	 *  if the size method works
+	 */
+	@Test	
+	public void testSize() {
+		SetADT<String> size_z= new JavaSet();
+		SetADT<String> size_n= new JavaSet();
+		SetADT<String> size_in =new JavaSet();
+		SetADT<String> size_l =new JavaSet();
+		size_n.insert("Hello");
+		size_in.insert("Hello");
+		size_in.insert("World");
+			size_l.insert("Hello");
+			size_l.insert("World");
+			size_l.insert(",");
+			size_l.insert("this");
+			size_l.insert("is");
+			size_l.insert( "not");
+			size_l.insert("a");
+			size_l.insert("sentence");
+			
+		// assert equals to false should not crash given
+		// the lhs has a false statement
+		assertEquals(size_z.size(), 0);
+		assertEquals(size_z.size()==10, false);
+		assertEquals(size_n.size(), 1);	
+		assertEquals(size_n.size()==2, false);
+		assertEquals(size_l.size(), 8);
+		assertEquals(size_l.size()==2, false);
+
+}			
+	/**
+	 *  Test that insert method is working
+	 *  The test should have no errors or failures
+	 *  if the insert method works
+	 */
+	@Test	
+	public void testInsert() {
+		SetADT<String> Insert= new JavaSet();
+		Insert.insert("Added");
+		assertEquals(Insert.size()==1, true);
+		assertEquals(Insert.size()==2, false);
+
+	}
+	
+	/**
+	 *  Test that contains method is working
+	 *  The test should have no errors or failures
+	 *  if the contains method works
+	 */
+	@Test
+	public void testContains() {
+		SetADT<String> Contains= new JavaSet();
+		Contains.insert("Red");
+		Contains.insert("Orange");
+		Contains.insert("Yellow");
+		Contains.insert("Green");
+		Contains.insert("Blue");
+		Contains.insert("Indigo");
+		Contains.insert("Violet");
+		assertEquals(Contains.size()==7,true);
+		assertEquals(Contains.size()==10,false);
+
+	}
+	
+	/**
+	 *  Test that remove method is working
+	 *  The test should have no errors or failures
+	 *  if the remove method works
+	 */
+	@Test
+	public void testRemove() {
+		SetADT<String> Remove= new JavaSet();
+		Remove.insert("dogs");
+		Remove.insert("cats");
+		Remove.insert("pigs");
+		Remove.insert("horse");
+		Remove.remove("horse");
+		assertEquals( Remove.size(), 3);
+		assertEquals( Remove.size()==2, false);
+
+	}
+	
 	
 }
